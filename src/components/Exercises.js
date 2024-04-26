3ddb44658da08995eaa24dd98ab4ed334d8e0c68
jstevenson02 <jacobryanstevenson@gmail.com>
2024-04-26 11:30:53 -0400

    update API

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -11,10 +11,7 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
 
   const indexOfLastExercise = currentPage * exercisesPerPage;
   const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage;
-  const currentExercises = exercises.slice(
-    indexOfFirstExercise,
-    indexOfLastExercise
-  );
+  const currentExercises = exercises.slice(indexOfFirstExercise, indexOfLastExercise);
 
   const paginate = (e, value) => {
     setCurrentPage(value);
@@ -43,30 +40,30 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
   }, [bodyPart]);
 
   return (
-    <Box id="exercises" sx={{ mt: { lg: "110px" } }} mt="50px" p="20px">
-      <Typography variant="h3" mb="46px">
+    <Box id='exercises' sx={{ mt: { lg: "110px" } }} mt='50px' p='20px'>
+      <Typography variant='h3' mb='46px'>
         Showing Results
       </Typography>
       <Stack
-        direction="row"
+        direction='row'
         sx={{ gap: { lg: "110px", xs: "50px" } }}
-        flexWrap="wrap"
-        justifyContent="center"
+        flexWrap='wrap'
+        justifyContent='center'
       >
         {currentExercises.map((exercise, index) => (
           <ExerciseCard key={index} exercise={exercise} />
         ))}
       </Stack>
-      <Stack mt="100px" alignItems="center">
+      <Stack mt='100px' alignItems='center'>
         {exercises.length > 9 && (
           <Pagination
-            color="standard"
-            shape="rounded"
+            color='standard'
+            shape='rounded'
             defaultPage={1}
             count={Math.ceil(exercises.length / exercisesPerPage)}
             page={currentPage}
             onChange={paginate}
-            size="large"
+            size='large'
           />
         )}
       </Stack>
